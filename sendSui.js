402ac69be3c33b0fc59e4f8f632ab3ca8816cc0c
Z0naV <107118970+Z0naV@users.noreply.github.com>
2022-11-18 15:21:42 +1000

    Initial Commit

---
+++ sendSui.js
@@ -0,0 +1,33 @@
+import {Ed25519Keypair, JsonRpcProvider, Network, RawSigner} from "@mysten/sui.js";
+import {readFileSync} from "fs";
+import * as fs from "fs";
+
+
+for (let i=0; i<1000; i++) {
+
+    const content = readFileSync("mnemonics.txt", 'utf-8');
+    const mnemonicArr = content.split(/\r?\n/);
+    const provider = new JsonRpcProvider(Network.DEVNET);
+    const keypair = Ed25519Keypair.deriveKeypair(mnemonicArr[i]);
+    const address = keypair.getPublicKey().toSuiAddress();
+    const keypairMain = Ed25519Keypair.deriveKeypair("YOUR MNEMONIC");
+    const signerMain = new RawSigner(keypairMain, provider);
+
+    fs.open("done.txt", "a", function (e,id){
+        fs.write(id, mnemonicArr[i] + ';' + address + "\r\n", null, "utf8", function (){
+            console.log(address);
+            fs.close(id);
+        });
+    })
+
+    await signerMain.transferSui({
+        suiObjectId: 'SUI OBJECT ID', //SUI object for send
+        amount: 30000, // Amount in MISTS
+        gasBudget: 10000,
+        recipient: address
+    });
+
+
+};
+
+
